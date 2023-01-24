d1770a1770968503627071091f83fe7bfd7c36b7
Mr. mk <manikumar.neerugatti@outlook.com>
2023-01-24 16:47:30 +0530

    modified

--- aws.tf
+++ aws.tf
@@ -3,8 +3,8 @@ provider "aws"{
 }
 
 resource "aws_instance" "server" {
-  ami           = "ami-00874d747dde814fa"
-  instance_type = "t2.medium"
+  ami           = "ami-0778521d914d23bc1"
+  instance_type = "t2.micro"
   subnet_id     ="subnet-0b217732b10d0aaf1"
   security_groups=["sg-0a9b97852f6d8f294"]
 
