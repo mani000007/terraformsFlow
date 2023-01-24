78e7453b4c8fcae0e8b245b867bb2d49550394b2
Mr. mk <manikumar.neerugatti@outlook.com>
2023-01-24 17:39:23 +0530

    final commit

--- aws.tf
+++ aws.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region= "us-east-1"
 }
 resource "aws_instance" "server" {
-  ami           = "ami-0778521d914d23bc1"
+  ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0b217732b10d0aaf1"
-  security_groups= ["sg-0a9b97852f6d8f294"]
+  subnet_id     = "subnet-0f5e1db05acd752b2"
+  security_groups= ["sg-05b3f1a6fd83be07c"]
   tags = {
     Name = "terraform"
   }
