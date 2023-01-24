0543b47137ce6547ac1c57313e36dbc0b9c80bfe
Mr. mk <manikumar.neerugatti@outlook.com>
2023-01-24 16:15:38 +0530

    update

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws"{
 resource "aws_instance" "server" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
-  subnet_id="subnet-084e48ad954a86134"
+  subnet_id="subnet-0b217732b10d0aaf1"
   security_groups=["sg-0a9b97852f6d8f294"]
 
   tags = {
