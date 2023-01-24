816ef7e8e63cb4b2fedacc24cba146df24390adc
Mr. mk <manikumar.neerugatti@outlook.com>
2023-01-24 16:06:39 +0530

    hello

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws"{
 }
 
 resource "aws_instance" "server" {
-  ami           = "ami-0778521d914d23bc1"
+  ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
   subnet_id="subnet-084e48ad954a86134"
   security_groups=["sg-0a9b97852f6d8f294"]
-  
+
   tags = {
     Name = "terraform-server"
   }
