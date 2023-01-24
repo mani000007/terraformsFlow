107f3002923e4ef3d98fce3e4fc90ecf363bdd77
Mr. mk <manikumar.neerugatti@outlook.com>
2023-01-24 16:36:46 +0530

    modify

--- aws.tf
+++ aws.tf
@@ -4,7 +4,7 @@ provider "aws"{
 
 resource "aws_instance" "server" {
   ami           = "ami-00874d747dde814fa"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id="subnet-0b217732b10d0aaf1"
   security_groups=["sg-0a9b97852f6d8f294"]
 
