f0d37646e52ea81d6c47ecf1b6c6c166fe49b2d4
Nmani <111077779+mani000007@users.noreply.github.com>
2023-01-24 16:40:35 +0530

    hii

--- aws.tf
+++ aws.tf
@@ -5,10 +5,10 @@ provider "aws"{
 resource "aws_instance" "server" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.medium"
-  subnet_id="subnet-0b217732b10d0aaf1"
+  subnet_id     ="subnet-0b217732b10d0aaf1"
   security_groups=["sg-0a9b97852f6d8f294"]
 
   tags = {
-    Name = "terraform-server"
+    Name = "terraform"
   }
 }
