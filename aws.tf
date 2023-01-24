2f8f8d80dad2470b062d7a8660150817b5096611
Mr. mk <manikumar.neerugatti@outlook.com>
2023-01-24 17:23:29 +0530

    modify

--- aws.tf
+++ aws.tf
@@ -1,11 +1,11 @@
-provider "aws"{
-    region="us-east-1"
+provider "aws" {
+    region= "us-east-1"
 }
 resource "aws_instance" "server" {
   ami           = "ami-0778521d914d23bc1"
   instance_type = "t2.micro"
-  subnet_id     ="subnet-0b217732b10d0aaf1"
-  security_groups=["sg-0a9b97852f6d8f294"]
+  subnet_id     = "subnet-0b217732b10d0aaf1"
+  security_groups= ["sg-0a9b97852f6d8f294"]
   tags = {
     Name = "terraform"
   }
