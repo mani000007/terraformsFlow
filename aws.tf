478a99a61aad45646ec3bcd55827f50ab6603b20
Nmani <111077779+mani000007@users.noreply.github.com>
2023-01-30 14:31:01 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -6,7 +6,15 @@ resource "aws_instance" "server" {
   instance_type = "t2.micro"
   subnet_id     = "subnet-0f5e1db05acd752b2"
   security_groups= ["sg-05b3f1a6fd83be07c"]
+  provisioner "remote-exec" {
+  inline = [
+    "sudo yum update -y",
+    "sudo yum install -y docker",
+    "sudo systemctl start docker",
+    "sudo docker run --name my-nginx -p 80:80 -d nginx"
+  ]
   tags = {
     Name = "terraform"
   }
+    
 }
