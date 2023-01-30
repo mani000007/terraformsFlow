dc37253e709990648a4a120c19219d599879c006
Nmani <111077779+mani000007@users.noreply.github.com>
2023-01-30 16:23:46 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -2,9 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_security_group" "example" {
   name        = "example"
   description = "Allow HTTP traffic"
+  vpc_id      = aws_vpc.example.id
 
   ingress {
     from_port   = 80
@@ -17,6 +22,5 @@ resource "aws_security_group" "example" {
 resource "aws_instance" "server" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
-
   vpc_security_group_ids = [aws_security_group.example.id]
 }
