49264041fac8986acea78beaff86b4d8f2927bbc
Nmani <111077779+mani000007@users.noreply.github.com>
2023-01-30 16:20:43 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    region= "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_security_group" "sg" {
-  name        = "instance_sg"
+resource "aws_security_group" "example" {
+  name        = "example"
   description = "Allow HTTP traffic"
 
   ingress {
@@ -17,6 +17,6 @@ resource "aws_security_group" "sg" {
 resource "aws_instance" "server" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0f5e1db05acd752b2"
-  security_groups = ["sg-05b3f1a6fd83be07c"]
+
+  vpc_security_group_ids = [aws_security_group.example.id]
 }
