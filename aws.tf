ed72331cb303b5f96e6ede25f84b926e6534fa64
Nmani <111077779+mani000007@users.noreply.github.com>
2023-01-30 15:35:30 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -5,14 +5,6 @@ resource "aws_instance" "server" {
   ami           = "ami-00874d747dde814fa"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0f5e1db05acd752b2"
-  security_groups= ["sg-05b3f1a6fd83be07c"]
-  provisioner "remote-exec" {
-  inline = [
-    "sudo apt update -y",
-    "sudo apt install -y docker",
-    "sudo systemctl start docker",
-    "sudo docker pull "${{ secrets.DOCKER_HUB_USERNAME }}/first-python"
-    "sudo docker run --name "${{ secrets.DOCKER_HUB_USERNAME }}/first-python" -p 80:80 -d first-python"
-  ]
+  security_groups= ["sg-05b3f1a6fd83be07c"
 }
 }
